65a78086be2b001d4f533d1eb288b31c3e7ac6a4
Daniel Galvez <dt.galvez@gmail.com>
2021-09-28 23:35:08 +0000

    WIP people's speech recipe.
    No DNN-HMM training yet.

--- egs/peoples_speech/s5/local/data_prep.py
+++ egs/peoples_speech/s5/local/data_prep.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import argparse
+from concurrent.futures import ThreadPoolExecutor
+import contextlib
 import glob
 import json
+import locale
 import os
 import subprocess
+import sys
 import tarfile
 from threading import Lock
 
 from g2p_en import G2p
+from smart_open import open
 import tqdm
 
 def remove_space(s: str):
@@ -20,13 +25,22 @@ def main():
     parser.add_argument('--data_dir',
                         required=True,
                         type=str)
+    parser.add_argument('--words_txt',
+                        required=True,
+                        type=str)
     parser.add_argument('--dict_dir',
                         required=True,
                         type=str)
     parser.add_argument('--input_dir',
                         required=True,
                         type=str)
+    parser.add_argument('--unk_string',
+                        required=True,
+                        type=str)
     parser.add_argument('--stage',
+                        required=True,
+                        type=int)
+    parser.add_argument('--nj',
                         required=True,
                         type=int)
 
@@ -40,21 +54,27 @@ def main():
     if stage <= 0:
         print("Create wav.scp file")
         os.makedirs(data_dir, exist_ok=True)
-        tar_file_names = glob.glob(os.path.join(input_dir, "tars/*.tar"))
-        tar_file_names = tar_file_names[:1]
+        tar_file_names = glob.glob(os.path.join(input_dir, "repartitioned_dataset_tars_jul_28_wav_new_join_no_space/*.tar"))
+        # tar_file_names = tar_file_names[:1]
         lock = Lock()
+        all_lines = []
         with open(os.path.join(data_dir, "wav.scp"), "w") as wav_scp_fh:
             def write_data(tar_file_name):
-                with tarfile.open(tar_file_name) as tar_fh:
+                with open(tar_file_name, mode="rb") as blob_fh, \
+                     tarfile.open(fileobj=blob_fh, mode="r") as tar_fh:
                     members = tar_fh.getmembers()
-                    lines = [f"{remove_space(member.name)} dd if=input.binary of=output.binary skip={member.offset_data} count={member.size} iflag=skip_bytes,count_bytes|\n"
+                    lines = [f"{remove_space(member.name)} dd if={tar_file_name} of=/dev/stdout skip={member.offset_data} count={member.size} iflag=skip_bytes,count_bytes |\n"
                              for member in members]
-                    lines = "".join(lines)
-                # May need to acquire the lock in a particular order...
+                    # lines = [f"{remove_space(member.name)} dd if=input.binary of=output.binary skip={member.offset_data} count={member.size} iflag=skip_bytes,count_bytes < tar_file_name | sox -t flac - -t wav -|\n"
+                    #          for member in members]
                 with lock:
-                    wav_scp_fh.write(lines)
-                
-            list(tqdm(executor.map(write_data, tar_file_names), total=len(tar_file_names)))
+                    all_lines.extend(lines)
+            executor = ThreadPoolExecutor(args.nj)
+            list(tqdm.tqdm(executor.map(write_data, tar_file_names), total=len(tar_file_names)))
+            all_lines.sort()
+            for line in all_lines:
+                wav_scp_fh.write(line)
+
             # for tar_file_name in tqdm.tqdm(tar_file_names):
             #     with tarfile.open(tar_file_name) as tar_fh:
             #         members = tar_fh.getmembers()
@@ -63,20 +83,32 @@ def main():
             #             #     print(f"Kaldi does not support keys with a space in them! Got {member.name}")
             #             name = remove_space(member.name)
             #             wav_scp_fh.write(f"{name} dd if=input.binary of=output.binary skip={member.offset_data} count={member.size} iflag=skip_bytes,count_bytes|\n")
+        del all_lines
 
     if stage <= 1:
         print("Create text file")
-        manifest_name,  = glob.glob(os.path.join(input_dir, "manifest/*.json"))
+        manifest_name,  = glob.glob(os.path.join(input_dir, "dataset_manifest_nemo_jul_28_wav_filtered_single_new_join_no_space/*.json"))
 
-        with open(os.path.join(data_dir, "text"), "w") as text_fh, \
-             open(os.path.join(data_dir, "utt2spk"), "w") as utt2spk_fh, \
-             open(manifest_name, "r") as manifest_fh:
+        with open(manifest_name, "r") as manifest_fh:
+            key_to_transcript = []
             for manifest_line in tqdm.tqdm(manifest_fh):
                 manifest_line = manifest_line.rstrip()
                 manifest_obj = json.loads(manifest_line)
                 key = remove_space(manifest_obj['audio_filepath'])
-                text_fh.write(f"{key} {manifest_obj['text']}\n")
+                key_to_transcript.append((key, manifest_obj['text']))
+
+        # key_to_transcript = [(k, v) for (k, v) in key_to_transcript.items()]
+        key_to_transcript.sort(key=lambda x: locale.strxfrm(x[0]))
+
+        with open(os.path.join(data_dir, "text"), "w") as text_fh, \
+             open(os.path.join(data_dir, "utt2spk"), "w") as utt2spk_fh:
+            for key, transcript in key_to_transcript:
+                text_fh.write(f"{key} {transcript}\n")
                 utt2spk_fh.write(f"{key} {key}\n")
+
+        # Conserve memory. This list is multiple gigabytes in size.
+        del key_to_transcript
+
         with open(os.path.join(data_dir, "utt2spk"), "r") as utt2spk_fh, \
              open(os.path.join(data_dir, "spk2utt"), "w") as spk2utt_fh:
             subprocess.check_call(["utils/utt2spk_to_spk2utt.pl"], stdin=utt2spk_fh, stdout=spk2utt_fh)
@@ -87,6 +119,21 @@ def main():
         word_to_phonemes = {}
         all_phonemes = set()
         g2p = G2p()
+
+        with open(args.words_txt) as words_fh:
+            for line in tqdm.tqdm(words_fh.readlines()):
+                word = line.rstrip()
+                if word in word_to_phonemes:
+                    continue
+                phonemes = g2p(word)
+                for phoneme in phonemes:
+                    if phoneme == " ":
+                        print(f"Offending word {word}: {phonemes}.\n in line: {line}", file=sys.stderr)
+                phonemes = [p for p in phonemes if p != " "]
+                word_to_phonemes[word] = phonemes
+                for phoneme in phonemes:
+                    all_phonemes.add(phoneme)
+
         with open(os.path.join(data_dir, "text"), "r") as text_fh:
             for line in tqdm.tqdm(text_fh.readlines()):
                 line = line.rstrip()
@@ -95,12 +142,18 @@ def main():
                     if word in word_to_phonemes:
                         continue
                     phonemes = g2p(word)
+                    for phoneme in phonemes:
+                        if phoneme == " ":
+                            print(f"Offending word {word}: {phonemes}.\n in line: {line}", file=sys.stderr)
+                    phonemes = [p for p in phonemes if p != " "]
                     word_to_phonemes[word] = phonemes
                     for phoneme in phonemes:
                         all_phonemes.add(phoneme)
 
+        # word_to_phonemes = [(k, v) for (k, v) in word_to_phonemes.items()]
+        # word_to_phonemes.sort(key=lambda x: locale.strxfrm(x[0]))
         with open(os.path.join(dict_dir, "words.txt"), "w") as dict_fh:
-            for word in word_to_phonemes.keys():
+            for word, _ in word_to_phonemes.items():
                 dict_fh.write(f"{word}\n")
 
         with open(os.path.join(dict_dir, "silence_phones.txt"), "w") as silence_phones_fh:
@@ -108,18 +161,29 @@ def main():
         with open(os.path.join(dict_dir, "optional_silence.txt"), "w") as optional_silence_fh:
             optional_silence_fh.write("SIL\n")
         with open(os.path.join(dict_dir, "nonsilence_phones.txt"), "w") as nonsilence_phones_fh:
-            for phoneme in sorted(phonemes):
+            for phoneme in sorted(all_phonemes):
                 nonsilence_phones_fh.write(f"{phoneme}\n")
-        # with open(os.path.join(dict_dir, "extra_questions.txt"), "w") as extra_questions_fh:
-        #     assert False
-        #     # extra_questions_fh.write()
+        with open(os.path.join(dict_dir, "extra_questions.txt"), "w") as extra_questions_fh:
+            extra_questions_fh.write("SIL SPN\n")
+            # extra_questions_fh.write("SIL_B SPN_B\n")
+            # extra_questions_fh.write("SIL_E SPN_E\n")
+            # extra_questions_fh.write("SIL_I SPN_I\n")
+            # extra_questions_fh.write("SIL_S SPN_S\n")
 
         with open(os.path.join(dict_dir, "lexicon.txt"), "w") as dict_fh:
-            dict_fh.write('!SIL SIL\n<spoken_noise> SPN\n<unk> SPN\n')
+            dict_fh.write(f'!SIL SIL\n<spoken_noise> SPN\n{args.unk_string} SPN\n')
             for word, phonemes in word_to_phonemes.items():
                 dict_fh.write(f"{word} {' '.join(phonemes)}\n")
 
     if stage <= 3:
+        # validate_data_dir.sh will complain if lexiconp.txt doesn't
+        # match lexicon.txt. However, if you are modifying the inputs
+        # to local/data_prep.py after having run
+        # utils/prepare_lang.sh, lexiconp.txt will have been created
+        # already by utils/prepare_lang.sh on the old lexicon.txt
+        # created by the previous run of data_prep.py
+        with contextlib.suppress(FileNotFoundError):
+            os.remove(os.path.join(dict_dir, "lexiconp.txt"))
         subprocess.check_call(["utils/validate_data_dir.sh", "--no-feats", data_dir])
         
 if __name__ == '__main__':
--- egs/peoples_speech/s5/run.sh
+++ egs/peoples_speech/s5/run.sh
@@ -4,53 +4,299 @@
 
 set -euo pipefail
 
-stage=0
+stage=1
 
-data=data/
+data=/mnt/disks/spark-scratch/peoples-speech/data
+mkdir -p $data
 exp=exp/
-input_dir=/gpfs/fs1/datasets/people-speech
+input_dir=/mnt/disks/spark-scratch/peoples-speech
+librispeech_data_dir=/mnt/disks/spark-scratch/librispeech
 
-. ./cmd.sh || exit 1;
-. ./path.sh || exit 1;
-. ./utils/parse_options.sh || exit 1;
+mkdir -p $librispeech_data_dir
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+
+unk="<unk>"
+test_sets="librispeech_dev_clean librispeech_dev_other librispeech_test_clean librispeech_test_other"
+
+function run_decode {
+  sys=$1
+  utils/mkgraph.sh $data/lang_test $exp/$sys $exp/$sys/graph
+
+  for test in $test_sets; do
+    num_lines=$(wc -l <data/$test/wav.scp)
+    decode_nj=$((num_lines / decode_audios_per_job + 1))
+
+    # this is an overly verbose way to pass extra options, but bash doesn't like the double "" around scoring-opts
+    if [ $cer = true ]; then
+      steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" --scoring-opts "--cer true" \
+          exp/$sys/graph data/$test exp/$sys/decode_$test &
+    else
+      steps/decode.sh --nj $decode_nj --cmd "$decode_cmd" exp/$sys/graph data/$test exp/$sys/decode_$test &
+    fi
+  done
+}
+
+# if [ $stage -le 0 ]; then
+#     apt-get update
+#     apt-get install -y python3-distutils
+#     wget https://bootstrap.pypa.io/get-pip.py
+#     python get-pip.py
+#     pip install tqdm g2p-en
+# fi
+
+dict_dir=$data/local/dict
+
+librispeech_data_url=www.openslr.org/resources/12
 
 if [ $stage -le 0 ]; then
-    apt-get update
-    apt-get install -y python3-distutils
-    wget https://bootstrap.pypa.io/get-pip.py
-    python get-pip.py
-    pip install tqdm gwp-en
+  for part in dev-clean test-clean dev-other test-other; do
+    local/librispeech_download_and_untar.sh $librispeech_data_dir $librispeech_data_url $part
+    local/librispeech_data_prep.sh $librispeech_data_dir/LibriSpeech/$part $data/$(echo "librispeech_${part}" | sed s/-/_/g)
+  done
 fi
 
 if [ $stage -le 1 ]; then
-  # format the data as Kaldi data directories
-  local/data_prep.py --data_dir=$data/train --input_dir=$input_dir --dict_dir=$data/local/dict_nosp --stage=1
-
-  # local/prepare_dict.sh --stage 3 --nj 30 --cmd "$train_cmd" \
-  #   data/local/lm data/local/lm data/local/dict_nosp
-
-  utils/prepare_lang.sh data/local/dict_nosp \
-    "<unk>" data/local/lang_tmp_nosp data/lang_nosp
-
-  # local/format_lms.sh --src-dir data/lang_nosp data/local/lm
-  # # Create ConstArpaLm format language model for full 3-gram and 4-gram LMs
-  # utils/build_const_arpa_lm.sh data/local/lm/lm_tglarge.arpa.gz \
-  #   data/lang_nosp data/lang_nosp_test_tglarge
+  :
+  # utils/build_const_arpa_lm.sh $data/local/lm/lm_tglarge.arpa.gz \
+  #    data/lang_nosp data/lang_nosp_test_tglarge
 fi
 
 if [ $stage -le 2 ]; then
-  mfccdir=mfcc
+  # format the data as Kaldi data directories
 
-  for part in train; do
-    steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 --write-utt2num-frames true --write-utt2dur true $data/$part $exp/make_mfcc/$part $mfccdir
-    steps/compute_cmvn_stats.sh $data/$part $exp/make_mfcc/$part $mfccdir
-  done
+  lm_dir=$data/local/lm
+  mkdir -p $lm_dir
 
-  # Get the shortest 500 utterances first because those are more likely
-  # to have accurate alignments.
-  # utils/subset_data_dir.sh --shortest data/train_clean_5 500 data/train_500short
+  pushd $lm_dir
+  # wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/tlt-jarvis/speechtotext_english_lm/versions/deployable_v1.0/zip -O speechtotext_english_lm_deployable_v1.0.zip
+  # unzip -o speechtotext_english_lm_deployable_v1.0.zip
+  cat 3-gram.pruned.3e-7.arpa | tr '[:upper:]' '[:lower:]' | gzip > 3-gram.pruned.3e-7.arpa.gz
+  # cp $dict_dir/words.txt $dict_dir/words.txt.backup
+  # TODO: Rerun g2p system on new words.txt!!!!
+  # grep -v '<eps>\|<s>\|</s>\|<unk>' 
+  gunzip -c 3-gram.pruned.3e-7.arpa.gz | arpa2fst --write-symbol-table=>(awk '{print $1}' | grep -v "<eps>\|<s>\|</s>\|$unk" > words.txt.tmp) - /dev/null
+  # cat <(awk '{print $1}' words.mixed_lm.3-gram.pruned.3e-7.txt) $dict_dir/words.txt | sort -u > $dict_dir/words.txt.tmp
+  # mv $dict_dir/words.txt.tmp $dict_dir/words.txt
+  popd
+
+  local/data_prep.py --data_dir=$data/train --input_dir=$input_dir --dict_dir=$dict_dir --words_txt=$lm_dir/words.txt.tmp --unk_string="$unk" --stag0=3 --nj=1 #$(($(nproc) * 10))
+
+  utils/prepare_lang.sh $dict_dir \
+    "$unk" $data/local/lang_tmp $data/lang
+
+  utils/format_lm.sh $data/lang $lm_dir/3-gram.pruned.3e-7.arpa.gz \
+                     $dict_dir/lexicon.txt $data/lang_test
+  
+  # local/format_lms.sh --src-dir data/lang data/local/lm
+  # # Create ConstArpaLm format language model for full 3-gram and 4-gram LMs
+
 fi
 
-# https://stackoverflow.com/a/40792605
+if [ $stage -le 3 ]; then
+    # train librispeech_dev_clean librispeech_dev_other librispeech_test_clean librispeech_test_other
+  for part in train $test_sets; do
+    mfccdir=$data/$part/mfcc
+    steps/make_mfcc.sh --cmd "$train_cmd" --nj $(nproc) --write-utt2num-frames true --write-utt2dur true $data/$part $exp/make_mfcc/$part $mfccdir
+    steps/compute_cmvn_stats.sh $data/$part $exp/make_mfcc/$part $mfccdir
+  done
+fi
 
-# dd if=input.binary of=output.binary skip=$offset count=$bytes iflag=skip_bytes,count_bytes
+if [ $stage -le 4 ]; then
+  # 4827136
+  total_num=$(wc -l <data/train/utt2spk)
+  subset_num=$((total_num/64))
+  utils/subset_data_dir.sh --shortest \
+    $data/train $subset_num $data/train_1d64
+  
+  subset_num=$((total_num/32))
+  utils/subset_data_dir.sh \
+    $data/train $subset_num $data/train_1d32
+
+  subset_num=$((total_num/16))
+  utils/subset_data_dir.sh \
+    $data/train $subset_num $data/train_1d16
+
+  subset_num=$((total_num/8))
+  utils/subset_data_dir.sh \
+    $data/train $subset_num $data/train_1d8
+  echo "======Subset train data END | current time : `date +%Y-%m-%d-%T`======="
+fi
+
+if [ $stage -le 5 ]; then
+  echo "======Train mono START | current time : `date +%Y-%m-%d-%T`============"
+  steps/train_mono.sh \
+      --boost-silence 1.25 --nj $train_nj --cmd "$train_cmd" \
+      $data/train_1d64 $data/lang $exp/mono
+  {
+    utils/mkgraph.sh $data/lang_test $exp/mono $exp/mono/graph
+    for part in $test_sets; do
+      [ ! -d $data/$part ] &&\
+        echo "$0: Decoder mono Error: no such dir $data/$part"
+      steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+        $exp/mono/graph $data/${part} $exp/mono/decode_${part}
+      cat $exp/mono/decode_${part}/wer_* | utils/best_wer.sh |\
+        sed "s/^/mono\t/" > $exp/mono/decode_${part}/wer.txt
+    done
+  } # &
+fi
+
+if [ $stage -le 6 ]; then
+  echo "======Train tri1 START | current time : `date +%Y-%m-%d-%T`==========="
+  steps/align_si.sh \
+    --boost-silence 1.25 --nj $train_nj --cmd "$train_cmd" \
+    $data/train_1d32 $data/lang \
+    $exp/mono $exp/mono_ali_train_1d32
+
+  steps/train_deltas.sh \
+    --boost-silence 1.25 --cmd "$train_cmd" 2000 10000 \
+    $data/train_1d32 $data/lang \
+    $exp/mono_ali_train_1d32 $exp/tri1
+  echo "======Train tri1 END | current time : `date +%Y-%m-%d-%T`============="
+  {
+    utils/mkgraph.sh $data/lang_test $exp/tri1 $exp/tri1/graph
+    for part in $test_sets; do
+      [ ! -d $data/$part ] &&\
+        echo "$0: Decoder tri1 Error: no such dir $data/$part" && exit 1;
+      steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+        $exp/tri1/graph $data/${part} $exp/tri1/decode_${part}
+      cat $exp/tri1/decode_${part}/wer_* | utils/best_wer.sh |\
+        sed "s/^/tri1\t/" > $exp/tri1/decode_${part}/wer.txt
+    done
+  } # &
+fi
+
+if [ $stage -le 7 ]; then
+  echo "======Train tri2 START | current time : `date +%Y-%m-%d-%T`==========="
+  steps/align_si.sh \
+    --nj $train_nj --cmd "$train_cmd" \
+    $data/train_1d16 $data/lang \
+    $exp/tri1 $exp/tri1_ali_train_1d16
+
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+    --splice-opts "--left-context=3 --right-context=3" 2500 15000 \
+    $data/train_1d16 $data/lang \
+    $exp/tri1_ali_train_1d16 $exp/tri2
+  echo "======Train tri2 END | current time : `date +%Y-%m-%d-%T`============="
+  {
+    utils/mkgraph.sh $data/lang_test $exp/tri2 $exp/tri2/graph || exit 1
+    for part in $test_sets; do
+      [ ! -d $data/$part ] &&\
+        echo "$0: Decoder tri2 Error: no such dir $data/$part" && exit 1;
+      steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+        $exp/tri2/graph $data/${part} $exp/tri2/decode_${part}
+      cat $exp/tri2/decode_${part}/wer_* | utils/best_wer.sh |\
+        sed "s/^/tri2\t/" > $exp/tri2/decode_${part}/wer.txt
+    done
+  } # &
+fi
+
+
+if [ $stage -le 8 ]; then
+  echo "======Train tri3b START | current time : `date +%Y-%m-%d-%T`==========="
+  steps/align_si.sh \
+    --nj $train_nj --cmd "$train_cmd" \
+    $data/train_1d8 $data/lang \
+    $exp/tri2 $exp/tri2_ali_train_1d8
+
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+    --num_iters 40 \
+    --splice-opts "--left-context=5 --right-context=5" 4500 35000 \
+    $data/train_1d8 $data/lang \
+    $exp/tri2_ali_train_1d8 $exp/tri3b
+  echo "======Train tri3b END | current time : `date +%Y-%m-%d-%T`============="
+  {
+    utils/mkgraph.sh $data/lang_test $exp/tri3b $exp/tri3b/graph || exit 1
+    for part in $test_sets; do
+      [ ! -d $data/$part ] &&\
+        echo "$0: Decoder tri3b Error: no such dir $data/$part" && exit 1;
+      steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+        $exp/tri3b/graph $data/${part} $exp/tri3b/decode_${part}
+      cat $exp/tri3b/decode_${part}/wer_* | utils/best_wer.sh |\
+        sed "s/^/tri3b\t/" > $exp/tri3b/decode_${part}/wer.txt
+    done
+  }
+fi
+
+if [ $stage -le 9 ]; then
+  echo "======Train tri4b START | current time : `date +%Y-%m-%d-%T`==========="
+  # steps/align_si.sh \
+  #   --nj $train_nj --cmd "$train_cmd" \
+  #   $data/train $data/lang \
+  #   $exp/tri3b $exp/tri3b_ali_train
+
+  # gmm-acc-mllt --rand-prune=4.0 exp//tri4b/2.mdl "ark,s,cs:apply-cmvn  --utt2spk=ark:/mnt/disks/spark-scratch/peoples-speech/data/train/split16/12/utt2spk scp:/mnt/disks/spark-scratch/peoples-speech/data/train/split16/12/cmvn.scp scp:/mnt/disks/spark-scratch/peoples-speech/data/train/split16/12/feats.scp ark:- | splice-feats --left-context=10 --right-context=10 ark:- ark:- | transform-feats exp//tri4b/0.mat" ark:- ark:- | ark:- exp//tri4b/2.12.macc
+
+  steps/train_lda_mllt.sh --cmd "$train_cmd" \
+    --stage 30 \
+    --num_iters 40 \
+    --splice-opts "--left-context=10 --right-context=10" 7500 75000 \
+    $data/train $data/lang \
+    $exp/tri3b_ali_train $exp/tri4b
+  echo "======Train tri4b END | current time : `date +%Y-%m-%d-%T`============="
+  {
+    utils/mkgraph.sh $data/lang_test $exp/tri4b $exp/tri4b/graph || exit 1
+    for part in $test_sets; do
+      [ ! -d $data/$part ] &&\
+        echo "$0: Decoder tri4b Error: no such dir $data/$part" && exit 1;
+      steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+        $exp/tri4b/graph $data/${part} $exp/tri4b/decode_${part}
+      cat $exp/tri4b/decode_${part}/wer_* | utils/best_wer.sh |\
+        sed "s/^/tri4b\t/" > $exp/tri4b/decode_${part}/wer.txt
+    done
+  }
+fi
+
+
+# if [ $stage -le 8 ]; then
+#   echo "======Train tri3 START | current time : `date +%Y-%m-%d-%T`==========="
+#   steps/align_si.sh \
+#     --nj $train_nj --cmd "$train_cmd" --use-graphs true \
+#     $data/train_1d16 $data/lang \
+#     $exp/tri2 $exp/tri2_ali_train_1d16
+
+#   steps/train_sat.sh \
+#     --cmd "$train_cmd" 2500 15000 \
+#     $data/train_1d16 $data/lang \
+#     $exp/tri2_ali_train_1d16 $exp/tri3
+#   echo "======Train tri3 END | current time : `date +%Y-%m-%d-%T`============="
+#   {
+#     utils/mkgraph.sh $data/lang_test $exp/tri3 $exp/tri3/graph
+#     for part in $test_sets; do
+#       [ ! -d $data/$part ] &&\
+#         echo "$0: Decoder tri3 Error: no such dir $data/$part" && exit 1;
+#       steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+#         $exp/tri3/graph $data/$part $exp/tri3/decode_${part}
+#       cat $exp/tri3/decode_${part}/wer_* | utils/best_wer.sh |\
+#         sed "s/^/tri3\t/" > $exp/tri3/decode_${part}/wer.txt
+#     done
+#   } # &
+# fi
+
+# if [ $stage -le 9 ]; then
+#   echo "======Train tri4 START | current time : `date +%Y-%m-%d-%T`==========="
+#   steps/align_fmllr.sh \
+#     --nj $train_nj --cmd "$train_cmd" \
+#     $data/train_1d8 $data/lang \
+#     $exp/tri3 $exp/tri3_ali_train_1d8
+
+#   steps/train_sat.sh \
+#     --cmd "$train_cmd" 4200 40000 \
+#     $data/train_1d8 $data/lang \
+#     $exp/tri3_ali_train_1d8 $exp/tri4
+#   echo "======Train tri4 END | current time : `date +%Y-%m-%d-%T`============="
+#   {
+#     utils/mkgraph.sh $data/lang_test $exp/tri4 $exp/tri4/graph || exit 1
+#     for part in $test_sets; do
+#       [ ! -d $data/$part ] &&\
+#         echo "$0: Decoder tri4 Error: no such dir $data/$part" && exit 1;
+#       steps/decode.sh --nj "$decode_nj" --cmd "$decode_cmd" \
+#         $exp/tri4/graph $data/${part} $exp/tri4/decode_${part}
+#       cat $exp/tri4/decode_${part}/wer_* | utils/best_wer.sh |\
+#         sed "s/^/tri4\t/" > $exp/tri4/decode_${part}/wer.txt
+#     done
+#   } # &
+# fi
+>
